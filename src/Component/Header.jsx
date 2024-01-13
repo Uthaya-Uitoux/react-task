41bc8e9c903a110a3dd2ceff4eb687436650f619
Uthaya-Uitoux <uthaya@uitoux.in>
2024-01-13 16:33:58 +0530

    code commited

---
+++ src/Component/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <tr className="header_sec">
+    <td id="logo">
+    <img src="src/assets/images/logo-dark.png" alt="logo" />
+    </td>
+    <td id="header_menu">
+    <h3>HOME</h3>
+    <h3>ABOUT US</h3>
+    <h3>FEATURES</h3>
+    <h3>CONTACT</h3>
+    </td>
+    </tr>
+
+    )
+}
+
+export default Header
